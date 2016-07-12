17dfe36ada29974dca5fdf766cd06fe322f8c226
litodaldegan <italodaldegan@hotmail.com>
2016-07-12 12:43:10 -0300

    Segundo exercicio feito

--- primeiro.py
+++ primeiro.py
@@ -1,7 +1,42 @@
-a = int (input ("Digie um numero: "))
+# Problema de fazer a soma de dois numeros
 
-if a > 2:
-	print ("Seu  numero: %f" %a)
+a = int (input ("Digite o primeiro numero: "))
+b = int (input ("Digite o segundo numero: "))
 
-else:
-	print ("abaixo de 2")
+print ("Soma: %d" %(a+b))
+
+# Problema de transformar dias, horas e minutos em segundos
+
+c = int (input ("Digite dias: "))
+d = int (input ("Digite horas: "))
+e = int (input ("Digite minutos: "))
+
+c *= 24 * 60 * 60
+d *= 60 * 60
+e *= 60
+
+print ("Total em segundos: %d" %(c+d+e))
+
+# Problema de calcular aumento de salario
+
+f = float (input ("Digite o salario atual: "))
+h = float (input ("Digite o percentual de aumento: "))
+
+print ("Novo salario: %f" %(f* (1+ h/100)))
+
+# Problema de calcular o tempo de viagem
+
+km = int (input ("Digite a distancia total: "))
+av = int (input ("Digite a velocidade media: "))
+
+print ("Tempo total de viagem: %dhoras" %(km/av))
+
+# Problema de dizer quantos digitos 2 elevado a um milhao tem
+
+x = pow(2,1000000)
+
+x2 = str(x)
+
+x3 = len(x2)
+
+print ("tamanho de 2 elevado a um milhao: %d" %x3)
--- segundo.py
+++ segundo.py
@@ -1,14 +1,40 @@
+# Problema do ano bixesto
 
-notas = [10,
-		15,
-		20,
-		40]
+year = int (input ("Digite um ano para verificar se e bixest0: "))
 
-x = 0
-soma = 0
+if year % 4 == 0:
+	if year / 100 > 0:
+		if year / 400 > 0:
+			print ("O ano e bixesto.")
+else:
+	print ("Ano nao bixesto")
 
-while x < 4:
-	soma += notas[x]
-	x += 1
+# Problema do total de pescado
 
-print ("Media: %d" %(soma/x))
+peso = int (input ("Digite o peso total do peixes: "))
+multa = 0
+acima = 0
+
+if peso > 50:
+	acima = peso - 50
+	print ("Peso acima da regulamentacao: %f" %acima)
+	print ("Multa: R$%d" %(acima / 4))
+else:
+	print ("Sem multa.")
+
+# Problema das latas de tinta
+
+a = int (input ("Digite a area total a ser pintada: "))
+cobertura = 3 * 18
+
+if a % cobertura == 0:
+	total = a / cobertura
+else:
+	total = (a / cobertura) + 1 
+
+print ("Total de latas de tintas neccessarias: %d" %total)
+
+
+
+
+
